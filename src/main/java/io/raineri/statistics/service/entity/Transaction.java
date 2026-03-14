75ffe5bfa1c4c38e71212b002cf9b3156a5fa24b
Marcelo Raineri <mraineridev@gmail.com>
2026-03-14 19:12:00 -0300

    refactor: update Transaction class to use final fields and improve validation checks

--- src/main/java/io/raineri/statistics/service/entity/Transaction.java
+++ src/main/java/io/raineri/statistics/service/entity/Transaction.java
@@ -3,6 +3,32 @@ package io.raineri.statistics.service.entity;
 import java.time.OffsetDateTime;
 
 public class Transaction {
-    private Integer value;
-    private OffsetDateTime date;
+    final private int value;
+    final private OffsetDateTime date;
+
+    public Transaction(int value, OffsetDateTime date) {
+        checkDate(date);
+        checkValue(value);
+        this.value = value;
+        this.date = date;
+    }
+
+    private void checkDate(OffsetDateTime date) {
+        if (date == null) {
+            // Error Handler
+        }
+        if(date.isAfter(OffsetDateTime.now())) {
+            // Error Handler
+        }
+
+        if(date.isEqual(OffsetDateTime.now())) {
+            // Error Handler
+        }
+    }
+
+    private void checkValue(int value) {
+        if(value < 0) {
+            // Error Handler
+        }
+    }
 }
