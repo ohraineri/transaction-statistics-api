effdf3501c5784861366a8eb1bf911dd8964ff00
Marcelo Raineri <mraineridev@gmail.com>
2026-03-14 14:13:21 -0300

    start project

---
+++ src/test/java/io/raineri/statistics/service/TransactionStatisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.raineri.statistics.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionStatisticsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
