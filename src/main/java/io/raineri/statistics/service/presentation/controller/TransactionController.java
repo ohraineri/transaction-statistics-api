4a97d7643a7d569b2f4fba60b963b637d0aeac20
Marcelo Raineri <mraineridev@gmail.com>
2026-03-18 21:36:53 -0300

    feat: implement TransactionController for transaction management

---
+++ src/main/java/io/raineri/statistics/service/presentation/controller/TransactionController.java
@@ -0,0 +1,38 @@
+package io.raineri.statistics.service.presentation.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.raineri.statistics.service.presentation.dto.RequestTransaction;
+import io.raineri.statistics.service.presentation.mapper.TransactionMapper;
+import io.raineri.statistics.service.application.usecase.contract.AddTransactionImpl;
+import io.raineri.statistics.service.application.usecase.contract.RemoveTransactionImpl;
+
+@RestController
+public class TransactionController {
+    private AddTransactionImpl addTransaction;
+    private RemoveTransactionImpl removeTransaction;
+
+    public TransactionController(AddTransactionImpl addTransaction, RemoveTransactionImpl removeTransaction) {
+        this.addTransaction = addTransaction;
+        this.removeTransaction = removeTransaction;
+    }
+
+    public TransactionController(AddTransactionImpl addTransaction) {
+        this.addTransaction = addTransaction;
+    }
+
+    @PostMapping("/transacao")
+    public void store(@Validated @RequestBody RequestTransaction request) {
+        this.addTransaction.execute(TransactionMapper.toEntity(request));
+    }
+
+    @DeleteMapping("/transacao")
+    public void delete() {
+        this.removeTransaction.execute();
+    }
+
+}
