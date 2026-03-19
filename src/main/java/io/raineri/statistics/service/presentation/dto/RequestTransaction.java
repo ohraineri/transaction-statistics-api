46b7f1235702afcf69dab48e74f4ba48062f2daa
Marcelo Raineri <mraineridev@gmail.com>
2026-03-18 22:01:12 -0300

    refactor: replace Lombok annotations with explicit getter and setter methods in RequestTransaction class

--- src/main/java/io/raineri/statistics/service/presentation/dto/RequestTransaction.java
+++ src/main/java/io/raineri/statistics/service/presentation/dto/RequestTransaction.java
@@ -1,18 +1,30 @@
 package io.raineri.statistics.service.presentation.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.OffsetDateTime;
 
-@Getter
-@Setter
 public class RequestTransaction {
     @JsonProperty("valor")
     private Double value;
     @JsonProperty("dataHora")
     @DateTimeFormat(iso =  DateTimeFormat.ISO.DATE_TIME)
     private OffsetDateTime date;
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public OffsetDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(OffsetDateTime date) {
+        this.date = date;
+    }
 }
