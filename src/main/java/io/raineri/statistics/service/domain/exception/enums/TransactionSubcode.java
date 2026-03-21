7802f39692fdb4953a93eb7d54304fccdef297b1
Marcelo Raineri <mraineridev@gmail.com>
2026-03-21 16:01:28 -0300

    feat: update TransactionSubcode messages for clarity and consistency

--- src/main/java/io/raineri/statistics/service/domain/exception/enums/TransactionSubcode.java
+++ src/main/java/io/raineri/statistics/service/domain/exception/enums/TransactionSubcode.java
@@ -3,9 +3,10 @@ package io.raineri.statistics.service.domain.exception.enums;
 import io.raineri.statistics.service.domain.exception.SubcodeException;
 
 public enum TransactionSubcode implements SubcodeException {
-    NULL_DATE("No date was given"),
-    FUTURE_DATE("The date shown is in the future."),
-    PRESENT_DATE("Data presents this in the present tense.");
+    NULL_DATE("Transaction date must not be null."),
+    FUTURE_DATE("Transaction date cannot be in the future."),
+    PRESENT_DATE("Transaction date cannot be the current moment."),
+    NEGATIVE_VALUE("Transaction value must be greater than or equal to zero.");
 
     private final String message;
 
