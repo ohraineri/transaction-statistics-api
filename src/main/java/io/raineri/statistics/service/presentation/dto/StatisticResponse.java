00f36419fbc342a6d8ca4ee8dc9d7f04cbc06cd0
Marcelo Raineri <mraineridev@gmail.com>
2026-03-20 21:42:40 -0300

    feat: add StatisticResponse DTO for statistical data representation

---
+++ src/main/java/io/raineri/statistics/service/presentation/dto/StatisticResponse.java
@@ -0,0 +1,49 @@
+package io.raineri.statistics.service.presentation.dto;
+
+public class StatisticResponse {
+    private double count;
+    private double sum;
+    private double avg;
+    private double min;
+    private double max;
+
+    public double getCount() {
+        return count;
+    }
+
+    public void setCount(double count) {
+        this.count = count;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+
+    public double getAvg() {
+        return avg;
+    }
+
+    public void setAvg(double avg) {
+        this.avg = avg;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public void setMin(double min) {
+        this.min = min;
+    }
+
+    public double getMax() {
+        return max;
+    }
+
+    public void setMax(double max) {
+        this.max = max;
+    }
+}
