b09fb07a8edc53c0bee464391b614d6d1bef9c9b
Marcelo Raineri <mraineridev@gmail.com>
2026-03-20 21:33:21 -0300

    refactor: update TransactionMapper to use TransactionRequest DTO

--- src/main/java/io/raineri/statistics/service/presentation/mapper/TransactionMapper.java
+++ src/main/java/io/raineri/statistics/service/presentation/mapper/TransactionMapper.java
@@ -1,10 +1,10 @@
 package io.raineri.statistics.service.presentation.mapper;
 
 import io.raineri.statistics.service.domain.entity.Transaction;
-import io.raineri.statistics.service.presentation.dto.RequestTransaction;
+import io.raineri.statistics.service.presentation.dto.TransactionRequest;
 
 public class TransactionMapper {
-    static public Transaction toEntity(RequestTransaction requestTransaction) {
-        return new Transaction(requestTransaction.getValue().intValue(), requestTransaction.getDate());
+    static public Transaction toEntity(TransactionRequest transactionRequest) {
+        return new Transaction(transactionRequest.getValue().intValue(), transactionRequest.getDate());
     }
 }
