2cc051eeb5e421d9a4fcfa4aea4acc4e3d8dce95
Marcelo Raineri <mraineridev@gmail.com>
2026-03-20 22:17:22 -0300

    feat: implement StatisticsGateway in StatisticsGatewayImpl for data retrieval by date range

--- src/main/java/io/raineri/statistics/service/infrastructure/gateway/StatisticsGatewayImpl.java
+++ src/main/java/io/raineri/statistics/service/infrastructure/gateway/StatisticsGatewayImpl.java
@@ -1,12 +1,15 @@
 package io.raineri.statistics.service.infrastructure.gateway;
 
+import io.raineri.statistics.service.application.gateway.StatisticsGateway;
 import io.raineri.statistics.service.infrastructure.database.Database;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 
-public class StatisticsGatewayImpl {
-    public ArrayList<Integer> getDataByDate(OffsetDateTime startDate, OffsetDateTime endDate) {
+public class StatisticsGatewayImpl implements StatisticsGateway {
+
+    @Override
+    public ArrayList<Integer> getValuesByDateRange(OffsetDateTime startDate, OffsetDateTime endDate) {
         return Database.whereDate(startDate, endDate);
     }
 }
