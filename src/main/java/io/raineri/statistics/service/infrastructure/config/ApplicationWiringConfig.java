ad13796091823b7264eb00634a51e8c52954cdc1
Marcelo Raineri <mraineridev@gmail.com>
2026-03-20 22:17:15 -0300

    feat: add StatisticsGateway and RetrieveStatisticsImpl beans to ApplicationWiringConfig

--- src/main/java/io/raineri/statistics/service/infrastructure/config/ApplicationWiringConfig.java
+++ src/main/java/io/raineri/statistics/service/infrastructure/config/ApplicationWiringConfig.java
@@ -1,10 +1,14 @@
 package io.raineri.statistics.service.infrastructure.config;
 
+import io.raineri.statistics.service.application.gateway.StatisticsGateway;
 import io.raineri.statistics.service.application.gateway.TransactionGateway;
 import io.raineri.statistics.service.application.usecase.AddTransaction;
+import io.raineri.statistics.service.application.usecase.RetrieveStatistics;
 import io.raineri.statistics.service.application.usecase.RemoveTransaction;
 import io.raineri.statistics.service.application.usecase.contract.AddTransactionImpl;
 import io.raineri.statistics.service.application.usecase.contract.RemoveTransactionImpl;
+import io.raineri.statistics.service.application.usecase.contract.RetrieveStatisticsImpl;
+import io.raineri.statistics.service.infrastructure.gateway.StatisticsGatewayImpl;
 import io.raineri.statistics.service.infrastructure.gateway.TransactionGatewayImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,5 +29,15 @@ public class ApplicationWiringConfig {
     @Bean
     public RemoveTransactionImpl removeTransaction(TransactionGateway transactionGateway) {
         return new RemoveTransaction(transactionGateway);
+    }
+
+    @Bean
+    public StatisticsGateway statisticsGateway() {
+        return new StatisticsGatewayImpl();
+    }
+
+    @Bean
+    public RetrieveStatisticsImpl retrieveStatistics(StatisticsGateway statisticsGateway) {
+        return new RetrieveStatistics(statisticsGateway);
     }
 }
