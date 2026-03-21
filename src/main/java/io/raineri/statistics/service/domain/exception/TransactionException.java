e763a2de25e5482adc36e5610d890a9978d73025
Marcelo Raineri <mraineridev@gmail.com>
2026-03-21 16:25:03 -0300

    refactor: remove unused import of TransactionSubcode in TransactionException

--- src/main/java/io/raineri/statistics/service/domain/exception/TransactionException.java
+++ src/main/java/io/raineri/statistics/service/domain/exception/TransactionException.java
@@ -1,7 +1,6 @@
 package io.raineri.statistics.service.domain.exception;
 
 import io.raineri.statistics.service.domain.exception.enums.DomainCode;
-import io.raineri.statistics.service.domain.exception.enums.TransactionSubcode;
 
 public class TransactionException extends DomainException {
     public TransactionException(TransactionSubcode subcode) {
